a3597ff1ff6ff9b1225816a4088adbda0d9c7146
kenisdee <156679025+kenisdee@users.noreply.github.com>
2024-08-01 18:51:29 +0400

    Add files via upload

---
+++ suite_12_3.py
@@ -0,0 +1,41 @@
+import unittest
+from tests_12_1 import RunnerTest
+from tests_12_2 import TournamentTest
+
+# Декоратор для пропуска тестов
+def skip_frozen(method):
+    def wrapper(self, *args, **kwargs):
+        if self.is_frozen:
+            self.skipTest('Тесты в этом кейсе заморожены')
+        else:
+            return method(self, *args, **kwargs)
+    return wrapper
+
+# Добавляем атрибут is_frozen в классы
+RunnerTest.is_frozen = False
+TournamentTest.is_frozen = True
+
+# Применяем декоратор к методам классов, кроме @classmethod
+for name, method in vars(RunnerTest).items():
+    if callable(method) and not name.startswith('__') and not name.startswith('setUp') and not name.startswith('tearDown'):
+        setattr(RunnerTest, name, skip_frozen(method))
+
+for name, method in vars(TournamentTest).items():
+    if callable(method) and not name.startswith('__') and not name.startswith('setUp') and not name.startswith('tearDown'):
+        setattr(TournamentTest, name, skip_frozen(method))
+
+# Создаем объект TestLoader
+loader = unittest.TestLoader()
+
+# Создаем объект TestSuite
+test_suite = unittest.TestSuite()
+
+# Добавляем тесты из RunnerTest и TournamentTest в TestSuite с использованием loadTestsFromTestCase
+test_suite.addTests(loader.loadTestsFromTestCase(RunnerTest))
+test_suite.addTests(loader.loadTestsFromTestCase(TournamentTest))
+
+# Создаем объект TextTestRunner с аргументом verbosity=2
+runner = unittest.TextTestRunner(verbosity=2)
+
+# Запускаем TestSuite
+runner.run(test_suite)
--- tests_12_2.py
+++ tests_12_2.py
@@ -1,26 +1,5 @@
 import unittest
-
-class Runner:
-    def __init__(self, name, speed=5):
-        self.name = name
-        self.distance = 0
-        self.speed = speed
-
-    def run(self):
-        self.distance += self.speed * 2
-
-    def walk(self):
-        self.distance += self.speed
-
-    def __str__(self):
-        return self.name
-
-    def __eq__(self, other):
-        if isinstance(other, str):
-            return self.name == other
-        elif isinstance(other, Runner):
-            return self.name == other.name
-        return False
+from runner_and_tournament import Runner
 
 class Tournament:
     def __init__(self, distance, *participants):
